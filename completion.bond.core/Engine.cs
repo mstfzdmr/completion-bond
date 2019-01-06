566c4c03777df1361feee318f78388e2eb79ce5d
Mustafa Ozdemir <mustafa.ozdemir@morhipo.com>
2019-01-06 11:47:41 +0300

    .

---
+++ completion.bond.core/Engine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace completion.bond.core
+{
+    public class Engine<TModel> : IEngine<TModel>
+    {
+        private readonly BlockingCollection<TModel> blockingCollection;
+
+        private readonly CancellationTokenSource cancellationTokenSource;
+
+        public Engine()
+        {
+            this.cancellationTokenSource = new CancellationTokenSource();
+            this.blockingCollection = new BlockingCollection<TModel>();
+        }
+
+        public void Add(TModel model)
+        {
+            this.blockingCollection.Add(model);
+        }
+
+        public void Run()
+        {
+            Task.Factory.StartNew(() => { }, TaskCreationOptions.LongRunning);
+        }
+
+        public void Stop()
+        {
+            this.cancellationTokenSource.Cancel();
+            this.blockingCollection.CompleteAdding();
+            this.blockingCollection.Dispose();
+        }
+    }
+}
---
+++ completion.bond.core/IEngine.cs
@@ -0,0 +1,9 @@
+﻿namespace completion.bond.core
+{
+    public interface IEngine<TModel>
+    {
+        void Add(TModel model);
+        void Run();
+        void Stop();
+    }
+}
